51e92abfb170e1a5f42f60b3ab52c0a42d6a3780
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-06-28 20:21:33 -0400

    Init

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,9 @@
+class WelcomesController < ApplicationController
+  def hello_method
+    render json: { message: "Hello from welcomes controller!" }
+  end
+
+  def about_method
+    render json: { message: "My favorite programming is Ruby because it's straightforward" }
+  end
+end
